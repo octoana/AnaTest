ee0554be75949c2e096c1ed236e39c614a42b056
Ana Scolari <127357173+apsscolari@users.noreply.github.com>
2025-02-11 20:12:23 -0800

    Create testOpenAI.py
    committing secret for testing the partner steps

---
+++ testOpenAI.py
@@ -0,0 +1,3 @@
+# for testing purposes if the secret will be recognized by scan, the resource is disabled to external access.
+key = 'api-key'
+token = '9KPsqaVCtK1oFBjZF3yBtUFZCqnX3ktmYvR97WLFNwYM54rNrHz4JQQJ99BBACYeBjFXJ3w3AAABACOGWv0x'
