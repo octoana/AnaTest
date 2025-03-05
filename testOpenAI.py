5f507c93def61048823b3561953499d01021d9b7
Ana Scolari <127357173+apsscolari@users.noreply.github.com>
2025-03-05 10:55:38 -0800

    Ana SCRUM-5 AnaTest #comment fixed this today
    Ana SCRUM-5 AnaTest #comment fixed this today testing Jira smart commits

--- testOpenAI.py
+++ testOpenAI.py
@@ -1,4 +1,5 @@
 # for testing purposes if the secret will be recognized by scan, the resource is disabled to external access.
 # secret expired and no longer working.
+# test ana
 key = 'api-key'
 token = 'xxxx'
